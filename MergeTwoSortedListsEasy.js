09b98de2420790df79389b8ddc3878153cb50313
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2019-07-15 06:28:24 -0700

    Create MergeTwoSortedListsEasy.js

---
+++ MergeTwoSortedListsEasy.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let head = new ListNode;
+    let curr = head;
+    
+    while(l1!=null && l2!=null) {
+        if(l1.val < l2.val) {
+            head.next = l1;
+            l1 = l1.next;
+        } else {
+            head.next = l2;
+            l2 = l2.next;
+        }
+        head = head.next;
+    }
+    head.next = (l1===null) ? l2 : l1;
+    return curr.next;
+};
